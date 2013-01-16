70ded100677b7e89d21e7614a62442d14442bd43
shycherry.vaio <vinpowful@free.fr>
2013-01-16 21:49:01 +0100

    add finder service

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ global.BABB = require('./config').config
 global.BABB.Libs = {
   Roms : require('roms'),
   Spawner : require('spawner'),
-  FilenamesFilter : require('filenamesFilter').FilenamesFilter
+  FilenamesFilter : require('filenamesFilter').FilenamesFilter,
+  Finder : require('finder')
 }  
 
 global.BABB.EventEmitter = new Backbone.View()
@@ -17,8 +18,17 @@ var Controller = require('controller')
 var Gui = require('nw.gui')
 
 function go(){  
-  Controller.doSniff()
+  Controller.doSniff()  
   //Gui.Window.get().show()
+  
+  // try{
+    // var find = global.BABB.Libs.Finder.find
+    // find('c:/', 'ePSXe.exe', function(fullpath){
+      // global.BABB.EventEmitter.trigger('info', 'ePSXe.exe found :'+fullpath)
+    // })
+  // }catch(err){
+    // global.BABB.EventEmitter.trigger('error', err.toString())    
+  // }
 }
 
 $(document).ready(go)
