4ac12c9cffb685cdc342fcfb8b092774bdee6a5c
vincent chicherie <vincent.chicherie@gmail.com>
2013-01-19 00:39:14 +0100

    add dummy platform for testing purposes

---
+++ platforms/Dummy MAME/platform.js
@@ -0,0 +1,36 @@
+var mamePath = 'C:/Users/Vincent/Desktop/Emulateur/mame/mame64.exe'
+var romsPaths = ["C:/Users/Vincent/Desktop/Roms/MAME"]
+
+exports.getName = function(){
+  return 'Dummy'
+}
+
+exports.getLogoPath = function(){
+  return __dirname+'/images/8364-haveac00kie-Mame.png'
+}
+
+exports.getRomsPaths = function(){
+  return romsPaths
+}
+
+exports.isAvailable = function(){
+  var Fs = require('fs')
+  return true
+}
+
+exports.runRom = function (iRom){
+  if(iRom){
+    var selectedRomPath = iRom.get('path')
+    if(selectedRomPath){      
+      var Spawner = global.BABB.Libs.Spawner
+      var Path = require('path')
+      Spawner.spawn(
+        mamePath, 
+        ['-rp', Path.dirname(selectedRomPath), iRom.get('title')],
+        {cwd : Path.dirname(mamePath)}
+      )        
+    }
+  }  
+}
+
+
