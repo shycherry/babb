aa093bedd8436844ef376e9ec4594d7137d324a8
shycherry.vaio <vinpowful@free.fr>
2013-01-26 14:57:12 +0100

    add Game Gear platform

---
+++ platforms/Game Gear/platform.js
@@ -0,0 +1,54 @@
+﻿var config = require('./config').config
+var emulatorPath = config.emulatorPath
+var romsPaths = config.romsPaths
+
+exports.getName = function(){
+  return config.displayName
+}
+
+exports.getLogoPath = function(){
+  return __dirname+'/images/logo.png'
+}
+
+exports.getRomsPaths = function(){
+  return romsPaths
+}
+
+exports.isAvailable = function(){
+  var Fs = require('fs')
+  return Fs.existsSync(emulatorPath)
+}
+
+exports.romsProvider = function(iReport, ioRomsCollection){
+  var Roms = global.BABB.Libs.Roms  
+  var FilenamesFilter = global.BABB.Libs.FilenamesFilter
+  
+  var filteredFilesMap = new FilenamesFilter(iReport)
+      .keepFilesWithExtensions(config.romsExtensions)
+      .onlyKeepBasename()
+      .removeExtensions()
+      .get()
+
+  for(var locPath in filteredFilesMap){
+    var rom = new Roms.Rom({
+      title : filteredFilesMap[locPath],
+      path : locPath
+    })    
+    ioRomsCollection.add(rom)
+  }  
+}
+
+exports.runRom = function (iRom){  
+  if(iRom){  
+    var selectedRomPath = iRom.get('path')
+    if(selectedRomPath){
+      var Spawner = global.BABB.Libs.Spawner
+      var Path = require('path')
+      Spawner.spawn(
+        emulatorPath, 
+        [selectedRomPath], 
+        {cwd : Path.dirname(emulatorPath)}
+      )      
+    }
+  }  
+}
