712700ceb1e2e718defe5f462003595086e5609c
shycherry.vaio <vinpowful@free.fr>
2013-01-26 10:43:38 +0100

    add windows platform

---
+++ platforms/Windows/platform.js
@@ -0,0 +1,35 @@
+var mamePath = 'notepad.exe'
+var execsPaths = [__dirname+'/Execs']
+
+exports.getName = function(){
+  return 'Windows'
+}
+
+exports.getLogoPath = function(){
+  return __dirname+'/images/Windows_logo.png'
+}
+
+exports.getRomsPaths = function(){
+  return execsPaths
+}
+
+exports.isAvailable = function(){
+  var Fs = require('fs')
+  return true
+}
+
+exports.runRom = function (iRom){
+  if(iRom){
+    if(iRom.get('title') == 'Retourner A Windows'){
+      process.exit()
+    }    
+    
+    var selectedRomPath = iRom.get('path')
+    if(selectedRomPath){
+      var Spawner = global.BABB.Libs.Spawner      
+      Spawner.exec(selectedRomPath)
+    }
+  }    
+}
+
+
