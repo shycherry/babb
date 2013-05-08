6dea8ac77a3bce4e43911998896566c49a1236ab
shycherry.vaio <vinpowful@free.fr>
2013-05-08 17:22:59 +0200

    wip

--- platforms/_BABB Stats/platform.js
+++ platforms/_BABB Stats/platform.js
@@ -1,23 +1,12 @@
+var BABBPlatform = global.BABB.coreRequire('platforms').Platform
 
-exports.getViewName = function(){
-  return 'BABBStats'
-}
-
-exports.getName = function(){
-  return 'BABB Stats'
-}
-
-exports.getLogoPath = function(){
-  return __dirname+'/logo.png'
-}
-
-exports.isAvailable = function(){
-  var Fs = require('fs')
-  return true
-}
-
-exports.runRom = function (iPlatform, iRom){
+exports.Platform = BABBPlatform.extend({
   
-}
-
-
+  isAvailable : function(){
+    return true
+  },
+  
+  runRom : function (iPlatform, iRom){    
+  },
+  
+})
