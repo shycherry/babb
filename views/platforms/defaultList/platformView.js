4ec494e149522180e42f08aaafd5a68ec8651ef2
Vincent Chicherie <vincent.chicherie@gmail.com>
2013-06-15 12:38:38 +0200

    add defaultList platformView
    for bro Asselin ;)

---
+++ views/platforms/defaultList/platformView.js
@@ -0,0 +1,59 @@
+﻿var BABB = global.BABB
+var Fs = require('fs')
+var BasePlatformView = BABB.platformsViewsRequire('base').PlatformView
+
+exports.PlatformView = BasePlatformView.extend({
+  
+  getCoverflowTemplatePath : function(){
+    return __dirname+'/item-template.html'
+  },
+  
+  getHtmlCoverElement : function(iRom){
+    return $('#images')
+  },
+  
+  recreateGraphicalRomList : function(iRomsCollection){
+    if(iRomsCollection){
+      this.romsCollection = iRomsCollection
+    }
+    
+    var Coverflow = BABB.coreRequire('coverflow')
+    var cellWidth = window.innerWidth/4
+    var coverflowModel = new Coverflow.CoverflowModel({
+      orientation : 'vertical',
+      template : _.template(Fs.readFileSync(this.getCoverflowTemplatePath()).toString()),
+      height: window.innerHeight,      
+      width : cellWidth,
+      left : cellWidth/2,
+      top : window.innerHeight/2,
+      perspective : 0,
+      cellWidth : cellWidth,
+      cellHeight : window.innerHeight/10,
+      coverGap : window.innerHeight/10,
+      coverOffset : 0,
+      zUnselected : -cellWidth/2,
+      circularSelection : true,
+      rotateAngleLeft : 0,
+      rotateAngleRight : 0,
+      virtualSize:10,
+      collection : this.romsCollection,
+    })
+
+    this.coverflowView = new Coverflow.CoverflowView({
+      el:'#romscoverflow',
+      model : coverflowModel
+    })    
+    
+    var self = this
+    this.coverflowView.on('focus', function(iRom){
+      BABB.EventEmitter.trigger('romFocused', iRom)      
+    })
+    
+    this.coverflowView.on('clicked', function(iRom){
+      BABB.EventEmitter.trigger('romValidated', iRom)
+    })
+    
+    this.dynabodyPlatform = null
+    this.coverflowView.select(this.lastSelectedRomId)    
+  },
+})
