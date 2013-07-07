31904473fea771d8afff7b8baedf683f0338af21
shycherry.vaio <vinpowful@free.fr>
2013-07-07 16:56:37 +0200

    add dreamcast platform

---
+++ platforms/Dreamcast/platform.js
@@ -0,0 +1,25 @@
+﻿var BABBPlatform = global.BABB.coreRequire('platforms').Platform
+
+exports.Platform = BABBPlatform.extend({
+  runRom : function (iPlatform, iRom){
+    if(iRom){
+      var emulatorPath = this.getPlatformConfig().emulatorPath
+      var selectedRomPath = iRom.get('path')
+      if(selectedRomPath){
+        var Spawner = global.BABB.Utils.Spawner
+        var Path = require('path')
+        Spawner.spawn(
+          emulatorPath,
+          [          
+          '-config', 'nullDC:Emulator.Autostart=1',
+          '-config', 'ImageReader:LoadDefaultImage=1',
+          '-config', 'ImageReader:DefaultImage='+selectedRomPath
+          ],
+          {cwd : Path.dirname(emulatorPath)},
+          iPlatform,
+          iRom
+        )
+      }
+    }
+  }
+})
