507de13ad008aca9c6efff0e34f9ecfba0872fc2
shycherry.vaio <vinpowful@free.fr>
2013-07-10 22:22:33 +0200

    prepare platforms to launchers delegation

---
+++ core/launchers.js
@@ -0,0 +1,89 @@
+﻿var $ = global.$
+var Backbone = global.Backbone
+var _ = global._
+var BABB = global.BABB
+
+var Path = require('path')
+var Fs = require('fs')
+
+var Launcher = Backbone.Model.extend({
+  defaults: {
+    id : "x",
+    name : "unammed launcher",
+    path : "/default/path"
+  },
+
+  _launcherConfig : null,
+
+  initialize: function(){
+    this.set('id', this.cid)
+
+    if(this.getLauncherConfig().displayName){
+      this.set('name', this.getLauncherConfig().displayName)
+    }
+  },
+
+  getLauncherConfig : function(){
+    if(!this._launcherConfig){
+      var configPath = Path.normalize(this.get('path')+'/config')
+      if(Fs.existsSync(configPath)){
+        this._launcherConfig = require(configPath).LauncherConfig
+      }else{
+        this._launcherConfig = {}
+      }
+    }
+    return this._launcherConfig
+  },
+
+  getLogoPath : function(){
+    if(this.getLauncherConfig().logoPath){
+      return this.getLauncherConfig().logoPath
+    }else{
+      return this.get('path')+Path.sep+'logo.png'
+    }
+  },
+
+  getShadowConfig: function(){
+    if(this.getLauncherConfig().ShadowConfig){
+      return this.getLauncherConfig().ShadowConfig
+    }
+    return null
+  },
+
+  runRom : function (iPlatform, iRom){
+    console.log('using default runRom')
+    if(iRom){
+      var emulatorPath = this.getLauncherConfig().emulatorPath
+      var selectedRomPath = iRom.get('path')
+      if(selectedRomPath){
+        var Spawner = global.BABB.Utils.Spawner
+        var Path = require('path')
+        Spawner.spawn(
+          emulatorPath,
+          [selectedRomPath],
+          {cwd : Path.dirname(emulatorPath)},
+          iPlatform,
+          iRom
+        )
+      }
+    }
+  },
+
+  isAvailable : function (){
+    var emulatorPath = this.getLauncherConfig().emulatorPath
+    return Fs.existsSync(emulatorPath)
+  },
+
+  toString: function(){
+    return this.get('name')+' ('+this.get('path')+')'
+  }
+})
+
+var LaunchersCollection = Backbone.Collection.extend({
+  model: Launcher,
+  initialize: function(){
+  }
+})
+
+exports.Launcher = Launcher
+exports.LaunchersCollection = LaunchersCollection
