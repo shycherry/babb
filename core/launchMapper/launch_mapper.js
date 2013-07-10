b74b84d4216bec5be8b50fba3d8e171b2270da5f
shycherry.vaio <vinpowful@free.fr>
2013-07-10 21:54:22 +0200

    add launcher mapper

---
+++ core/launchMapper/launch_mapper.js
@@ -0,0 +1,43 @@
+﻿var Config = global.BABB.LaunchMapperConfig
+var Fs = require('fs')
+var Path = require('path')
+var ChildProcess = require('child_process')
+var EventEmitter = global.BABB.EventEmitter
+
+
+var buildLaunchMapperPath = function(iRom, iPlatform){
+  var launchMapperPath = Config.savePath
+  if(iRom && iPlatform){
+    launchMapperPath += Path.sep+iRom.get('title')+'_'+iPlatform.get('name')
+  }
+  return launchMapperPath
+}
+
+var writeFile = function(iPath, data){
+  if( ! Fs.existsSync(Config.savePath)){
+    Fs.mkdirSync(Config.savePath)
+  }
+  if( ! Fs.existsSync(iPath)){
+    Fs.writeFileSync(iPath, data)
+  }
+
+}
+
+exports.mapLauncher = function(iRom, iPlatform, iLauncher){
+  if(!iPlatform){
+    return
+  }
+  var savePath = buildLaunchMapperPath(iRom, iPlatform)
+  writeFile(savePath, iLauncher.get('name'))
+}
+
+exports.getLauncher = function(iRom, iPlatform){
+  if(!iRom || !iPlatform){
+    return
+  }
+  var savePath = buildLaunchMapperPath(iRom, iPlatform)
+  if(Fs.existsSync(savePath)){
+    var launcherName = Fs.readFileSync(savePath).toString()
+    return iPlatform.getRunnerFromName(launcherName)
+  }
+}
