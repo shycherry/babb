7977e692ccb25aec30dc113ef28e2346ccae1ec5
shycherry <vincent.chicherie@gmail.com>
2014-04-21 14:58:19 +0200

    add DT ePSXe launcher

---
+++ platforms/PS One/DT ePSXe Launcher/launcher.js
@@ -0,0 +1,43 @@
+﻿var Fs = require('fs')
+var ChildProcess = require('child_process')
+var BABBLauncher = global.BABB.coreRequire('launchers').Launcher
+
+exports.Launcher = BABBLauncher.extend({
+  runRom : function (iPlatform, iRom){
+    if(iRom){
+      var emulatorPath = this.getLauncherConfig().emulatorPath
+      var daemonToolsPath = this.getLauncherConfig().daemonToolsPath
+
+      var selectedRomPath = iRom.get('path')
+      if(selectedRomPath){
+        var Spawner = global.BABB.Utils.Spawner
+        var Path = require('path')
+
+        var daemonToolsMounting = ChildProcess.spawn(
+          daemonToolsPath,
+          ['-mount', 'scsi,', '0,', selectedRomPath],
+          {cwd : Path.dirname(daemonToolsPath)}          
+        )
+        BABB.EventEmitter.trigger('info', 'mounting with daemon tools...')
+
+        daemonToolsMounting.on('exit', function(code){
+          BABB.EventEmitter.trigger('info', 'mounted')
+          Spawner.spawn(
+            emulatorPath,
+            ['-nogui'],
+            {cwd : Path.dirname(emulatorPath)},
+            iPlatform,
+            iRom
+          )
+        })        
+      }
+    }
+  },
+
+  isAvailable : function(){
+    var basicStuffAvailable = BABBLauncher.prototype.isAvailable.call(this)
+    var daemonToolsAvailable = Fs.existsSync(this.getLauncherConfig().daemonToolsPath)        
+    return (basicStuffAvailable && daemonToolsAvailable); 
+  }
+  
+})
